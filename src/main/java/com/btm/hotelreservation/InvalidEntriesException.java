f3a8024afd94194f4082df8651e16ddc39fad0e5
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-09 23:37:57 +0530

    Added:Code for finding cheapest rated Hotel For Rewardee using stream

---
+++ src/main/java/com/btm/hotelreservation/InvalidEntriesException.java
@@ -0,0 +1,9 @@
+package com.btm.hotelreservation;
+
+public class InvalidEntriesException extends Exception {
+
+	public InvalidEntriesException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
